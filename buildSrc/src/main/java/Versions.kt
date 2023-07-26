adaa3a6c824aa9702983c9a44f2cd945d0225cf6
Dawid Raszka <raszka.dawid@gmail.com>
2023-07-26 22:45:57 +0200

    Version 1.6.0-rc

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,11 +1,11 @@
 object Versions {
-    const val kotlin = "1.8.20"
+    const val kotlin = "1.9.0"
     const val detekt = "1.22.0"
     const val ktx = "1.8.0"
     const val appCompat = "1.5.0"
     const val ktlint = "10.3.0"
-    const val composeCompiler = "1.4.7"
-    const val compose = "1.5.0-beta01"
+    const val composeCompiler = "1.5.1"
+    const val compose = "1.5.0-rc01"
     const val material3 = "1.1.0"
     const val composeActivity = "1.7.2"
 }
